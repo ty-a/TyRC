320db97acd73a953d4259a252325f3d889373bd6
Ty <tyler@faceyspacies.com>
2017-04-07 14:05:02 -0500

    #1 basic thing
    though the feed is still likely to change

--- src/main/java/com/faceyspacies/TyRC/Containers/DiscussionsFeedEntry.java
+++ src/main/java/com/faceyspacies/TyRC/Containers/DiscussionsFeedEntry.java
@@ -1,15 +1,180 @@
 package com.faceyspacies.TyRC.Containers;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class DiscussionsFeedEntry {
 	
-	public enum Type {
+	public enum EntryType {
 		DISCUSSION_THREAD,
 		DISCUSSION_REPLY,
 		DISCUSSION_REPORT
 	}
+	
+	public enum Actions {
+		CREATED,
+		DELETED,
+		UNDELETED,
+		MOVED,
+		MODIFIED
+	}
+	
+	private EntryType type;
+	private Actions  action;
+	private String user;
+	private String url;
+	private String snippet;
+	private String title;
+	
 
-	public DiscussionsFeedEntry() {
-		// TODO Auto-generated constructor stub
+	public DiscussionsFeedEntry(String json) throws JSONException, Exception {
+		JSONObject jsonObject = new JSONObject(json);
+		
+		switch (jsonObject.getString("type")) {
+		case "discussion-thread":
+			setType(EntryType.DISCUSSION_THREAD);
+			try {
+				setTitle(jsonObject.getString("title"));
+			} catch (JSONException e) {
+				setTitle("");
+			}
+			break;
+			
+		case "discussion-post":
+			setType(EntryType.DISCUSSION_REPLY);
+			break;
+			
+		case "discussion-report":
+			setType(EntryType.DISCUSSION_REPORT);
+			break;
+			
+		default:
+			throw new Exception("Unknown Discussion type \"" + jsonObject.getString("type") + "\"");
+			
+		}
+		
+		switch(jsonObject.getString("action")) {
+		case "created":
+			setAction(Actions.CREATED);
+			break;
+			
+		case "deleted":
+			setAction(Actions.DELETED);
+			break;
+			
+		case "un-deleted":
+			setAction(Actions.UNDELETED);
+			break;
+			
+		case "moved":
+			setAction(Actions.MOVED);
+			break;
+			
+		default:
+			throw new Exception("Unknown Discussion action \"" + jsonObject.getString("action") + "\"");
+		}
+		
+		setUser(jsonObject.getString("userName"));
+		setUrl(jsonObject.getString("url"));
+		
+		if(!(getType() == EntryType.DISCUSSION_THREAD))
+			setSnippet(jsonObject.getString("snippet"));
+		
+		
+	}
+
+	/**
+	 * @return the type
+	 */
+	public EntryType getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(EntryType type) {
+		this.type = type;
+	}
+
+
+	/**
+	 * @return the action
+	 */
+	public Actions getAction() {
+		return action;
+	}
+
+
+	/**
+	 * @param action the action to set
+	 */
+	public void setAction(Actions action) {
+		this.action = action;
+	}
+
+
+	/**
+	 * @return the user
+	 */
+	public String getUser() {
+		return user;
+	}
+
+
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+
+	/**
+	 * @return the snippet
+	 */
+	public String getSnippet() {
+		return snippet;
+	}
+
+
+	/**
+	 * @param snippet the snippet to set
+	 */
+	public void setSnippet(String snippet) {
+		this.snippet = snippet;
+	}
+
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
 }
--- src/main/java/com/faceyspacies/TyRC/Listeners/WikiaRCListener.java
+++ src/main/java/com/faceyspacies/TyRC/Listeners/WikiaRCListener.java
@@ -1,8 +1,11 @@
 package com.faceyspacies.TyRC.Listeners;
 
+import org.json.JSONException;
 import org.pircbotx.MultiBotManager;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
+
+import com.faceyspacies.TyRC.Containers.DiscussionsFeedEntry;
 
 public class WikiaRCListener extends ListenerAdapter {
 
@@ -15,6 +18,99 @@ public class WikiaRCListener extends ListenerAdapter {
 	@Override
 	public void onMessage(MessageEvent e) {
 		System.out.println(e.toString());
+		try {
+			DiscussionsFeedEntry in = new DiscussionsFeedEntry(e.getMessage());
+			
+			String out = "[[";
+		
+			out += in.getUser() + "]]";
+			
+			switch (in.getType()) {
+			case DISCUSSION_REPLY:
+				switch(in.getAction()) {
+				case CREATED:
+					out += " replied ";
+					break;
+				case DELETED:
+					out += " deleted reply ";
+					break;
+				case UNDELETED:
+					out += " undeleted reply ";
+					break;
+				case MOVED:
+					// will never get here
+					throw new Exception("Action MOVED with a DISCUSSION_REPLY. Should not happen.");
+				case MODIFIED:
+					out += " edited reply ";
+					break;
+				default:
+					break;
+				}
+				
+				out += in.getUrl() + " : ";
+				
+				out += in.getSnippet();
+				break;
+			case DISCUSSION_REPORT:
+				switch(in.getAction()) {
+				case CREATED:
+					out += " reported post ";
+					break;
+				case DELETED:
+					out += " deleted report ";
+					break;
+				case UNDELETED:
+					out += " undeleted report ";
+					break;
+				case MOVED:
+					// will never get here
+					throw new Exception("Action MOVED with a DISCUSSION_REPORT. Should not happen.");
+				case MODIFIED:
+					out += " edited report "; // can this even happen?
+					break;
+				default:
+					break;
+				}
+				
+				out += in.getUrl() + " : ";
+				
+				out += in.getSnippet();
+				break;
+			case DISCUSSION_THREAD:
+				switch(in.getAction()) {
+				case CREATED:
+					out += " created thread [[" + in.getTitle() + "]] ";
+					break;
+				case DELETED:
+					out += " deleted thread [[" + in.getTitle() + "]] ";
+					break;
+				case UNDELETED:
+					out += " undeleted thread [[" + in.getTitle() + "]] ";
+					break;
+				case MOVED:
+					out += " moved thread [[" + in.getTitle() + "]] ";
+				case MODIFIED:
+					out += " edited thread [[" + in.getTitle() + "]] ";
+					break;
+				default:
+					break;
+				}
+				out += in.getUrl();
+				
+				break;
+			}
+			
+			
+			
+			manager.getBotById(0).send().message("#tybot", out);
+			
+		} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 	}
 }
